64973e3c6bc25960c0b45e3b20d0f22bdb03d3b3
SivamaheshAllam <82816689+SivamaheshAllam@users.noreply.github.com>
2023-03-13 20:33:50 +0530

    Update App.js

--- project/src/App.js
+++ project/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <h1>This heading is added by Manjunadh from Local Repository</h1>
     <h1>This is the heading added by other developer</h1>
-    <h1>This is heading, added by sivamaheshallam </h1>
+    <h1>This is the heading, added by sivamaheshallam </h1>
     <h1>this is heading added by Sunil kumar sahoo </h1>
     <h1>This is the fifth headings added by Imran Hussain from Local Repository</h1>
     </div>
