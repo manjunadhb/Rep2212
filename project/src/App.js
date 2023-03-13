7eba8c8cdad0f86d4d9cecaa5b520f7ac8e55648
manjunadhb <manjunadhb@gmail.com>
2023-03-13 15:33:45 +0530

    added another heading

--- project/src/App.js
+++ project/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>This heading is added by Manjunadh from Local Repository</h1>
+    <h1>This is the heading added by other developer</h1>
     </div>
   );
 }
