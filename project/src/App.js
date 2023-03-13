a88200d98d0510b01884ab613344805a4c15ddf9
SivamaheshAllam <sivamahesh.edu@gmail.com>
2023-03-13 21:16:11 +0530

    App.js modified

--- project/src/App.js
+++ project/src/App.js
@@ -5,14 +5,17 @@ function App() {
   return (
     <div className="App">
       <h1>This heading is added by Manjunadh from Local Repository</h1>
-    <h1>This is the heading added by other developer</h1>
+      <h1>This is the heading added by other developer</h1>
 
-    <h1>This is heading, added by sivamaheshallam </h1>
-    <h1>This is the heading, added by sivamaheshallam </h1>
-    <h1>this is heading added by Sunil kumar sahoo </h1>
-    <h1>This is the fifth headings added by Imran Hussain from Local Repository</h1>
-    <h1>This is the heading added by SivamaheshAllam from local Repository</h1>
-
+      <h1>This is heading, added by sivamaheshallam </h1>
+      <h1>This is the heading, added by sivamaheshallam </h1>
+      <h1>this is heading added by Sunil kumar sahoo </h1>
+      <h1>
+        This is the fifth headings added by Imran Hussain from Local Repository
+      </h1>
+      <h1>
+        This is the heading added by SivamaheshAllam from local Repository
+      </h1>
     </div>
   );
 }
