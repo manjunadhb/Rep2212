6b6dc287f0e810269ad8b1872d19df396522551a
Md Imran Hussain <mdimranhusain47@gmail.com>
2023-03-14 13:57:41 +0530

    added audio and images files and created audio sections

--- project/src/App.js
+++ project/src/App.js
@@ -17,6 +17,38 @@ function App() {
         This is the heading added by SivamaheshAllam from local Repository
       </h1>
       <h1>This is the heading added by Imran Hussain from github/remote repository</h1>
+
+      <h1 style={{color: "blue"}}>Audio sections</h1>
+      <div className="audioContainer">
+      <img src="./images/man_meri_jaan_song.jpg"></img>
+      <h2>Title: Mann Meri Jaan</h2>
+      <audio src="./audio/man_meri_jaan.mp3" controls muted></audio>
+    </div>
+
+    <div className="audioContainer">
+      <img src="./images/pathaan_poster.jpg"></img>
+      <h2>Title: Jhoome Jo Pathaan</h2>
+      <audio src="./audio/jhume_jo_pathaan.mp3" controls muted></audio>
+    </div>
+
+    <div className="audioContainer">
+      <img src="./images/Manike-From-Thank-God.jpg"></img>
+      <h2>Title: Manike_Mange</h2>
+      <audio src="./audio/manike_songs.mp3" controls muted></audio>
+    </div>
+
+    <div className="audioContainer">
+      <img src="./images/meri_jindahi_h_tu.jpg"></img>
+      <h2>Title: Meri Jindagi hai</h2>
+      <audio src="./audio/Meri_jindagi_hai_tu.mp3" controls muted></audio>
+    </div>
+
+    <div className="audioContainer">
+      <img src="./images/Pyaar-Karte-Ho-Na.jpg"></img>
+      <h2>Title: Pyar karte ho na</h2>
+      <audio src="./audio/pyar karte ho na.mp3" controls muted></audio>
+    </div>
+
     </div>
   );
 }
